c265f162a63f45cb89cf23749278b4acb7c2b8a5
Fabian Carvajal Murillo <inbox@edgcarmu.me>
2019-07-26 18:09:21 -0600

    update first commit

--- src/app/resources/lang/en/crgeodata.php
+++ src/app/resources/lang/en/crgeodata.php
@@ -2,44 +2,13 @@
 
 /*
 |--------------------------------------------------------------------------
-| Bamboo\Accounting Language Lines
+| edgcarmu\crgeodata Language Lines
 |--------------------------------------------------------------------------
 |
 */
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | ID Types
-    |--------------------------------------------------------------------------
-    |
-    */
-    'id_types' => [
-        'menu_title' => 'ID Types',
-        'singular' => 'ID Type',
-        'plural' => 'ID Types',
-        'id' => 'id',
-        'name' => 'name',
-        'validation_regx' => 'validation regx',
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Companies
-    |--------------------------------------------------------------------------
-    |
-    */
-    'companies' => [
-        'menu_title' => 'Companies',
-        'singular' => 'company',
-        'plural' => 'companies',
-        'legal_name' => 'name',
-        'identification' => 'identification',
-        'phone' => 'phone',
-        'email' => 'email',
-        'website' => 'web site',
-        'decimal_accuracy' => 'Decimal Accuracy',
-    ],
+    'provincia' => 'provincia',
 
 ];
